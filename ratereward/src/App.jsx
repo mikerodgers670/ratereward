839cfe3c15a56281bd1e366cce39bba142cc4e2a
mikerodgers11670 <157884155+mikerodgers670@users.noreply.github.com>
2026-04-15 21:56:24 +0300

    Update App.jsx

--- ratereward/src/App.jsx
+++ ratereward/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import RegisterPage from './pages/RegisterPage'
+import UserStatus from './pages/UserStatus'
 import DashboardPage from './pages/DashboardPage'
 import AdminLoginPage from './pages/AdminLoginPage'
 import AdminDashboard from './pages/AdminDashboard'
@@ -10,6 +11,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/status" element={<UserStatus />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
